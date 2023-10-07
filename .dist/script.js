0c692443c8a360ec1df55520b5c73fc02ccb1635
Izabela Ilvan <you@example.com>
2023-10-07 20:08:22 +0300

    final changes

--- .dist/script.js
+++ .dist/script.js
@@ -1,1 +1,72 @@
-console.log('testing')
+let apiQuotes = []; //empty array
+
+//correspondent constant for each of the elements
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterButton = document.getElementById('twitter');
+const newQuoteButton = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
+
+function newQuote() {
+    showLoadingSpinner();
+    //Pick a random quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    //populate our text content of the author and quote element
+    //authorText.textContent = quote.author;         //set the value
+    //check if author field is blank and replace it with 'Unknown'
+    if (!quote.author) {
+        author.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;         //set the value
+    }
+
+    //Check Quote length to determine styling
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    //set quote, hide loader
+    quoteText.textContent = quote.text;
+    removeLoadingSpinner();
+}
+
+// Get quotes from API
+async function getQuotes() {
+    showLoadingSpinner();
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json(); //getting json from api and then pass that in apiQuotes which is a global variable
+        newQuote();
+    } catch (error) {
+        getQuotes();
+    }
+}
+
+//Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank'); //open a new tab
+
+}
+
+//Event Listeners
+newQuoteButton.addEventListener('click', newQuote);
+twitterButton.addEventListener('click', tweetQuote);
+
+//On Load
+getQuotes();
